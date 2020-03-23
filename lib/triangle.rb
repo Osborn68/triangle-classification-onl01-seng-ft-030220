2258e141ee942a2bc4edb1a7a58062f1eb9dc02e
Christopher Osborn <z68ozz@gmail.com>
2020-03-23 01:51:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,32 @@
 class Triangle
   # write code here
   
-  attr_accessor :sides
+ 
   
-  def initialize(one, two, three)
-    @sides = [one, two, three]
+  def initialize(a, b, c)
+    @a = a 
+    @b = b
+    @c = c
   end 
   
   def kind
+    if @a <= 0 || @b <= 0 || @c <= 0 
+      raise TriangleError
+      puts error.message
+      elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b 
+      raise TriangleError
+      puts error.message
+      elsif @a == @b && @b == @c && @a == @c
+      :equilateral
+      elsif @a == @b || @b == @c || @a == @c 
+      :isosceles
+      elsif @a != @b && @b != @c && @a != @c 
+      :scalene
+    end 
+  end 
+  
+  class TriangleError < StandardError
+    def message
+      
     
 end
